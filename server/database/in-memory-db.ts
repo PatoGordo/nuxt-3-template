0b81db1e6cad1744b8b065f2ecee0ab22a353441
PatoGordo <icariusv@gmail.com>
2022-12-30 01:37:35 -0300

    fixing a problem in the memory db

--- server/database/in-memory-db.ts
+++ server/database/in-memory-db.ts
@@ -1,4 +1,4 @@
-import { User } from "../../temp/app/domain/entities/User";
+import { User } from "../app/domain/entities/User";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const inMemoryDB: { users: User[] } = {
