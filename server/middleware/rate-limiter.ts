498e16d53256f998a040d8d69b4ed11efe3480e7
IcaroMiguel <conversecomicaro@gmail.com>
2023-01-24 16:56:35 -0300

    adding a rate limiter translations

--- server/middleware/rate-limiter.ts
+++ server/middleware/rate-limiter.ts
@@ -1,31 +1,35 @@
 import { RateLimiter } from "limiter";
-import { defineEventHandler, getRequestHeader, createError } from "h3";
+import { defineEventHandler, getRequestHeader } from "h3";
 import cache from "memory-cache";
+import { $st } from "../i18n/$st";
 
 // THIS RATE LIMITER ALLOWS 10 REQUESTS EVERY 10 SECONDS
 export default defineEventHandler(async (event) => {
-  const ip = getRequestHeader(event, "x-forwarded-for");
+  if (event.node.req.url?.includes("/api/")) {
+    const ip = getRequestHeader(event, "x-forwarded-for");
 
-  if (!cache.get(ip)) {
-    const cachedLimiter = new RateLimiter({
-      interval: 15000,
-      tokensPerInterval: 9,
-      fireImmediately: false,
-    });
-
-    cache.put(ip, cachedLimiter, 15000);
-  } else {
-    const cachedLimiter = cache.get(ip) as RateLimiter;
-
-    if (Math.floor(cachedLimiter.getTokensRemaining()) > 1) {
-      await cachedLimiter.removeTokens(1);
+    if (!cache.get(ip)) {
+      const cachedLimiter = new RateLimiter({
+        interval: 15000,
+        tokensPerInterval: 9,
+        fireImmediately: false,
+      });
 
       cache.put(ip, cachedLimiter, 15000);
     } else {
-      throw createError({
-        statusCode: 429,
-        statusMessage: "Too Many Requests",
-      });
+      const cachedLimiter = cache.get(ip) as RateLimiter;
+
+      if (Math.floor(cachedLimiter.getTokensRemaining()) > 1) {
+        await cachedLimiter.removeTokens(1);
+
+        cache.put(ip, cachedLimiter, 15000);
+      } else {
+        event.node.res.statusCode = 429;
+
+        return {
+          message: $st("alerts.too_many_requests"),
+        };
+      }
     }
   }
 });
